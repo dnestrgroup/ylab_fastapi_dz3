0647c8518ebbdb1f169c661854912c0cfb6ce923
Waldemar <dnestrgroup@gmail.com>
2023-08-10 15:15:37 +0300

    final commit dish

--- app/views/dishes.py
+++ app/views/dishes.py
@@ -1,13 +1,10 @@
-import json
-
-from fastapi import Depends, HTTPException
+from fastapi import Depends
 from fastapi.routing import APIRouter
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.db.base import get_db
-from app.db.redis.redis import redis_client
-from app.db.repositorie_for_dish import RepositoriesDishes
 from app.schemas.schemas import CreateDishesRequest, DishesResponse
+from app.services.service_dishes import DishesService
 
 router = APIRouter()
 
@@ -17,17 +14,8 @@ router = APIRouter()
     response_model=list[DishesResponse],
 )
 async def get_dishes(id_menu: int, id_submenu: int, db: AsyncSession = Depends(get_db)) -> list[DishesResponse]:
-    cached_data = redis_client.get(
-        f'/api/v1/menus/{id_menu}/submenus/{id_submenu}/dishes')
-    if cached_data:
-        list_dishes = json.loads(cached_data)
-        return [DishesResponse(**json.loads(dish)) for dish in list_dishes]
-    repo_d = RepositoriesDishes(db)
-    list_dishes = await repo_d.get_list(id_submenu)
-    dishes_for_redis = [dish.json() for dish in list_dishes]
-    redis_client.setex(
-        '/api/v1/menus/{id_menu}/submenus/{id_submenu}/dishes', 1000, json.dumps(dishes_for_redis))
-    return list_dishes
+    ds_service = DishesService(db=db)
+    return await ds_service.get_list_dishes(id_menu=id_menu, id_submenu=id_submenu)
 
 
 @router.get(
@@ -37,17 +25,8 @@ async def get_dishes(id_menu: int, id_submenu: int, db: AsyncSession = Depends(g
 async def get_dishes_one(
     id_menu: int, id_submenu: int, id_dishes: int, db: AsyncSession = Depends(get_db)
 ) -> DishesResponse:
-    cached_data = redis_client.get(
-        '/api/v1/menus/' + str(id_menu) + '/submenus/' + str(id_submenu) + '/dishes/' + str(id_dishes))
-    if cached_data:
-        return DishesResponse(**json.loads(cached_data))
-    repo_d = RepositoriesDishes(db)
-    res = await repo_d.get(id_menu, id_submenu, id_dishes)
-    if res:
-        redis_client.setex(
-            f'/api/v1/menus/{str(id_menu)}/submenus/{str(id_submenu)}/dishes/{ str(id_dishes)}', 1000, res.json())
-        return res
-    raise HTTPException(status_code=404, detail='dish not found')
+    ds_service = DishesService(db=db)
+    return await ds_service.get_one_dish_by_id(id_menu, id_submenu, id_dishes)
 
 
 @router.patch(
@@ -61,18 +40,8 @@ async def patch_dish(
     data: CreateDishesRequest,
     db: AsyncSession = Depends(get_db),
 ) -> DishesResponse:
-    repo_d = RepositoriesDishes(db)
-    res = await repo_d.update(id_menu, id_submenu, id_dishes, data)
-    # redis_client.flushall()
-    response_dish = DishesResponse(
-        id=res.id,
-        title=res.title,
-        description=res.description,
-        price=res.price
-    )
-    redis_client.setex(
-        f'/api/v1/menus/{str(id_menu)}/submenus/{str(id_submenu)}/dishes/{str(id_dishes)}', 1000, response_dish.json())
-    return res
+    ds_service = DishesService(db=db)
+    return await ds_service.update_dish(id_menu, id_submenu, id_dishes, data)
 
 
 @router.post(
@@ -86,19 +55,13 @@ async def post_dish(
     id_submenu: int,
     db: AsyncSession = Depends(get_db)
 ) -> DishesResponse:
-    repo_d = RepositoriesDishes(db)
-    res = await repo_d.create(data, id_menu, id_submenu)
-    if res:
-        redis_client.delete(f'/api/v1/menus/{id_menu}/submenus/{id_submenu}/dishes')
-        redis_client.setex(f'/api/v1/menus/{id_menu}/submenus/{id_submenu}/dishes/{res.id}', 1000, res.json())
-    return res
+    ds_service = DishesService(db=db)
+    return await ds_service.create_dish(data, id_menu, id_submenu)
 
 
 @router.delete(
     '/api/v1/menus/{id_menu}/submenus/{id_submenu}/dishes/{id_dishes}',
 )
 async def delete_dishes(id_menu: int, id_submenu: int, id_dishes: int, db: AsyncSession = Depends(get_db)) -> None:
-    repo_d = RepositoriesDishes(db)
-    redis_client.delete(f'/api/v1/menus/{str(id_menu)}/submenus/{str(id_submenu)}/dishes/{str(id_dishes)}')
-    redis_client.delete(f'/api/v1/menus/{str(id_menu)}/submenus/{str(id_submenu)}')
-    await repo_d.delete(id_dishes=id_dishes)
+    ds_service = DishesService(db=db)
+    await ds_service.delete_dish(id_menu, id_submenu, id_dishes)
