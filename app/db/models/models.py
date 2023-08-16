80d8d128319c826fe4a9777a14adcbac89e82258
Waldemar <dnestrgroup@gmail.com>
2023-08-16 18:54:06 +0300

    upd models.py with new xls_id

--- app/db/models/models.py
+++ app/db/models/models.py
@@ -6,30 +6,33 @@ from app.db.base import Base
 class MainMenu(Base):
     __tablename__ = 'main_menu'
     id = Column(Integer, primary_key=True)
+    id_xls = Column(String, nullable=True)
     title = Column(String, nullable=False)
     description = Column(String, nullable=False)
 
     __table_args__ = (
-        UniqueConstraint('title', name='uq_main_menu_title'),
+        UniqueConstraint('id_xls', name='uq_main_menu_id_xls'),
     )
 
 
 class SubMenu(Base):
     __tablename__ = 'sub_menu'
     id = Column(Integer, primary_key=True)
+    id_xls = Column(String, nullable=True)
     title = Column(String, nullable=False)
     description = Column(String, nullable=False)
     main_menu_id = Column(
         Integer, ForeignKey('main_menu.id', ondelete='CASCADE'), nullable=False
     )
     __table_args__ = (
-        UniqueConstraint('title', name='uq_sub_menu_title'),
+        UniqueConstraint('id_xls', name='uq_sub_menu_id_xls'),
     )
 
 
 class Dishes(Base):
     __tablename__ = 'dishes'
     id = Column(Integer, primary_key=True)
+    id_xls = Column(String, nullable=True)
     title = Column(String, nullable=False)
     description = Column(String, nullable=False)
     sub_menu_id = Column(
@@ -38,5 +41,5 @@ class Dishes(Base):
     price = Column(Numeric(precision=18, scale=2), nullable=True)
 
     __table_args__ = (
-        UniqueConstraint('title', name='uq_dishes_title'),
+        UniqueConstraint('id_xls', name='uq_dish_id_xls'),
     )
